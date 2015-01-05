4b638c019cd40864b747703abed4ba5560c77865
nbbenedict <noah@thebenedicts.net>
2015-01-05 01:44:03 -0700

    through step 9

--- js/HomeView.js
+++ js/HomeView.js
@@ -9,9 +9,16 @@ var HomeView = function(store) {
 			this.el.html(HomeView.template());
 			return this;
 		};
-		 this.findByName = function() {
+		this.findByName = function() {
 				store.findByName($('.search-key').val(), function(employees) {
 						$('.employee-list').html(HomeView.liTemplate(employees));
+						if (self.iscroll) {
+								console.log('Refresh iScroll');
+								self.iscroll.refresh();
+						} else {
+								console.log('New iScroll');
+								self.iscroll = new iScroll($('.scroll', self.el)[0], {hScrollbar: false, vScrollbar: false });
+						}
 				});
 		};
     this.initialize();
